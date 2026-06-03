53d38f2051f49a306476f394085a006425a85748
layja13 <luislayja1@gmail.com>
2026-06-02 20:44:47 -0600

    microbial ga finished - working properly - environments still can be unsolvable

--- maze-goal/main.py
+++ maze-goal/main.py
@@ -1,5 +1,5 @@
 from environment import Environment
-from solutions.genetic_algorithms import SimpleGeneticAlgorithm
+from solutions.genetic_algorithms import SimpleGeneticAlgorithm, MicrobialGeneticAlgorithm
 import matplotlib.pyplot as plt
 
 # -------------------------------------------------
@@ -27,19 +27,39 @@ POP_SIZE = 100
 NUM_GENES = 20
 NUM_GENERATIONS = 500
 
+"""
 # Simple Genetic Algorithm
 sga = SimpleGeneticAlgorithm(environment=environment, mutation_rate=MUTATION_RATE, pop_size=POP_SIZE, num_genes=NUM_GENES, num_generations=NUM_GENERATIONS)
 #sga.show_poblation(interpretable=True)
 
-
 solution = sga.evolve()
 
-
+print(f"Simple Genetic Algorithm (Population Based Search) Solution")
 print(f"Goal reached: {sga.win}")
 print(f"solution: {solution}")
-
 
 sga.show_interpretable_genome(solution)
 
 plt.plot([generation for generation in range(NUM_GENERATIONS)], sga.fitness_evolution_per_generation)
+plt.show()
+"""
 
+# ---------------------------------------------------------------------
+# Microbial Genetic Algorithm
+
+
+# Global variables
+SHARING_GENES_RATE = 0.3
+NUM_TOURNAMENTS = 2000
+
+mga = MicrobialGeneticAlgorithm(environment=environment, mutation_rate=MUTATION_RATE, pop_size=POP_SIZE, num_genes=NUM_GENES, num_tournaments=NUM_TOURNAMENTS, sharing_genes_rate=SHARING_GENES_RATE)
+
+solution = mga.evolve()
+print(f"Microbial Genetic Algorithm Solution")
+print(f"Goal reached: {mga.win}")
+print(f"solution: {solution}")
+
+mga.show_interpretable_genome(solution)
+
+plt.plot([tournament for tournament in range(NUM_TOURNAMENTS)], mga.fitness_evolution_per_generation)
+plt.show()
--- maze-goal/solutions/genetic_algorithms.py
+++ maze-goal/solutions/genetic_algorithms.py
@@ -32,6 +32,7 @@ class  SimpleGeneticAlgorithm:
             for candidate in self.population:
                 print(candidate)
     
+
     def show_interpretable_genome(self, genome):
         genome_interpretable = []
         for gen in genome:
@@ -41,15 +42,19 @@ class  SimpleGeneticAlgorithm:
 
 
     def _create_population_fitness(self):
-        poblation_fitness = []
+        population_fitness = []
+        self.population_steps = []
 
         for candidate in self.population:
-            poblation_fitness.append(self._evaluate_candidate(candidate))
+            fitness, steps = self._evaluate_candidate(candidate)
+
+            population_fitness.append(fitness)
+            self.population_steps.append(steps)
 
             self.agent_position_y = self.environment.agent_position_y
             self.agent_position_x = self.environment.agent_position_x
             
-        return poblation_fitness
+        return population_fitness
             
     
     def evolve(self):
@@ -68,8 +73,9 @@ class  SimpleGeneticAlgorithm:
             if best_fitness_generation > best_fitness:
                 best_fitness = best_fitness_generation
                 best_candidate_index = self.population_fitness.index(best_fitness_generation)
+                best_steps = self.population_steps[best_candidate_index]
 
-                best_candidate = self.population[best_candidate_index]
+                best_candidate = self.population[best_candidate_index][:best_steps]
 
 
             for candidate in range(self.pop_size):
@@ -149,12 +155,12 @@ class  SimpleGeneticAlgorithm:
             minimum_distance = min(minimum_distance, self._manhattan_distance())
 
             if self._game_on() == False:
-                return self._fitness(steps=steps, min_distance=minimum_distance, win=False)
+                return self._fitness(steps=steps, min_distance=minimum_distance, win=False), steps
             elif self.environment.environment[self.agent_position_y][self.agent_position_x] == "G":
                 self.win = True
-                return self._fitness(steps=steps, min_distance=minimum_distance, win=True)
+                return self._fitness(steps=steps, min_distance=minimum_distance, win=True), steps
             
-        return self._fitness(steps=steps, min_distance=minimum_distance, win=None)
+        return self._fitness(steps=steps, min_distance=minimum_distance, win=None), steps
 
 
     
@@ -177,3 +183,230 @@ class  SimpleGeneticAlgorithm:
     def _manhattan_distance(self):
         return abs(self.environment.goal_position_y - self.agent_position_y) + abs(self.environment.goal_position_x - self.agent_position_x)
     
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+# Microbial Genetic Algorithm
+
+class MicrobialGeneticAlgorithm:
+    def __init__(self, environment, mutation_rate, pop_size, num_genes, num_tournaments, sharing_genes_rate):
+        self.pop_size = pop_size
+        self.mutation_rate = mutation_rate
+        self.num_genes = num_genes
+        self.num_tournaments = num_tournaments
+        self.sharing_genes_rate = sharing_genes_rate
+        self.environment = environment
+        self.agent_position_y = self.environment.agent_position_y
+        self.agent_position_x = self.environment.agent_position_x
+        self.actions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        self.actions_interpretable = {(-1, 0):"UP", (1, 0):"DOWN", (0, 1):"RIGHT", (0, -1):"LEFT"}
+        self.win = False
+        self.fitness_evolution_per_generation = []
+        self.population = self._create_population()
+        self.population_fitness = self._create_population_fitness()
+
+
+    def _create_population(self):
+        return [[random.choice(self.actions) for gen in range(self.num_genes)] for candidate in range(self.pop_size)]
+    
+
+    def _create_population_fitness(self):
+        population_fitness = []
+        self.population_steps = []
+
+        for candidate in self.population:
+            fitness, steps = self._evaluate_candidate(candidate)
+
+            population_fitness.append(fitness)
+            self.population_steps.append(steps)
+
+            self.reset_agent_position()
+            
+        return population_fitness
+    
+            
+    def evolve(self):
+        highest_fitness = max(self.population_fitness)
+        best_candidate = self.population[self.population_fitness.index(highest_fitness)]
+
+        for tournament in range(self.num_tournaments):
+            #print(f"tournament {tournament}")
+
+            # selection
+            parent_one, index_parent_one = self._selection_tournament(num_candidates=2)
+            parent_two, index_parent_two = self._selection_tournament(num_candidates=2)
+
+            # cross-over
+            new_candidate = self._cross_over(parent_one, parent_two, index_parent_one, index_parent_two)
+
+            # mutation
+            new_candidate_mutated = self._mutation(new_candidate)
+
+            # Obtaining the fitness of the new candidate
+            fitness, steps = self._evaluate_candidate(new_candidate_mutated)
+
+            # reseting the agent's position
+            self.reset_agent_position()
+            
+            # Replacing the worst parent for the new candidate
+            if self.population_fitness[index_parent_one] > self.population_fitness[index_parent_two]:
+                self.population[index_parent_two] = new_candidate_mutated
+                self.population_fitness[index_parent_two] = fitness
+            else:
+                self.population[index_parent_one] = new_candidate_mutated
+                self.population_fitness[index_parent_one] = fitness
+            
+            if fitness > highest_fitness:
+                highest_fitness = fitness
+                best_candidate = new_candidate_mutated[:steps]
+            
+            self.fitness_evolution_per_generation.append(highest_fitness)
+        
+        return best_candidate
+
+
+    def reset_agent_position(self):
+        self.agent_position_y = self.environment.agent_position_y
+        self.agent_position_x = self.environment.agent_position_x
+
+
+    def _cross_over(self, parent_one, parent_two, index_parent_one, index_parent_two):
+        if self.population_fitness[index_parent_one] > self.population_fitness[index_parent_two]:
+            winner = parent_one
+            loser = parent_two
+        else:
+            winner = parent_two
+            loser = parent_one
+
+        shortest_parent = parent_one if len(parent_one) < len(parent_two) else parent_two
+
+        # sharing genes
+        for gen in range(len(shortest_parent)):
+            if random.random() > self.sharing_genes_rate:
+                loser[gen] = winner[gen]
+
+        return loser
+        
+    
+
+    def _mutation(self, child):
+        child_copy = child.copy()
+        
+        for gen in range(len(child)):
+            if random.random() < self.mutation_rate:
+                
+                new_gen = random.choice(self.actions)
+                while new_gen == child_copy[gen]:
+                    new_gen = random.choice(self.actions)
+
+                child_copy[gen] = new_gen
+
+        return child_copy
+    
+
+    def _selection_tournament(self, num_candidates=2):
+        indexes = []
+        for i in range(num_candidates):
+            indexes.append(random.randint(0, self.pop_size - 1))
+        
+        max_fitness = float("-inf")
+        max_index = float("-inf")
+
+        for index in indexes:
+            fitness = self.population_fitness[index]
+            if fitness > max_fitness:
+                max_fitness = fitness
+                max_index = index
+
+        return self.population[max_index], max_index
+
+
+    def show_population(self, interpretable=False):
+        if interpretable:
+            for candidate in self.population:
+                complete_interpretable_candidate = []
+                for gen in candidate:
+                    complete_interpretable_candidate.append(self.actions_interpretable[gen])
+                print(complete_interpretable_candidate)
+        else:  
+            for candidate in self.population:
+                print(candidate)
+
+
+    def _fitness(self, steps, min_distance, win):
+        area = self.environment.height*self.environment.width
+        if win:
+            return area - steps
+        elif win == False:
+            return -area -min_distance
+        else:
+            return -min_distance
+    
+
+
+    def _create_population_fitness(self):
+        population_fitness = []
+        self.population_steps = []
+
+        for candidate in self.population:
+            fitness, steps = self._evaluate_candidate(candidate)
+
+            population_fitness.append(fitness)
+            self.population_steps.append(steps)
+
+            self.agent_position_y = self.environment.agent_position_y
+            self.agent_position_x = self.environment.agent_position_x
+            
+        return population_fitness
+    
+
+    def show_interpretable_genome(self, genome):
+        genome_interpretable = []
+        for gen in genome:
+            genome_interpretable.append(self.actions_interpretable[gen])
+        
+        print(genome_interpretable)
+
+
+    def _evaluate_candidate(self, candidate):
+        steps = 0
+        minimum_distance = self._manhattan_distance()
+
+        for gen in candidate:
+            self._move(gen)
+            steps += 1
+            minimum_distance = min(minimum_distance, self._manhattan_distance())
+
+            if self._game_on() == False:
+                return self._fitness(steps=steps, min_distance=minimum_distance, win=False), steps
+            elif self.environment.environment[self.agent_position_y][self.agent_position_x] == "G":
+                self.win = True
+                return self._fitness(steps=steps, min_distance=minimum_distance, win=True), steps
+            
+        return self._fitness(steps=steps, min_distance=minimum_distance, win=None), steps
+
+    
+    def _move(self, gen):
+        dy = gen[0]
+        dx = gen[1]
+
+        self.agent_position_y += dy
+        self.agent_position_x += dx
+        return
+    
+
+    def _game_on(self):
+        if self.agent_position_y < 0 or self.agent_position_x < 0 or self.agent_position_y > self.environment.height - 1 \
+            or self.agent_position_x > self.environment.width - 1 or self.environment.environment[self.agent_position_y][self.agent_position_x] == "O":
+            return False
+        return True
+    
+    
+    def _manhattan_distance(self):
+        return abs(self.environment.goal_position_y - self.agent_position_y) + abs(self.environment.goal_position_x - self.agent_position_x)
+
+    
+
+
+
+
+
+        
